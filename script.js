b855e75874b207166b115582151f7158d05527af
Andreas Geckler <ageckler@web.de>
2022-11-19 22:26:17 +0100

    save

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function choose() {
-  lefttop.style.color = "red";
-  lefttop.innerHTML = "Choosen";
+  if (count % 2 == 0) {
+    lefttop.style.color = "red";
+    lefttop.innerHTML = "Player 1";
+    count++;
+  } else {
+    lefttop.style.color = "blue";
+    lefttop.innerHTML = "Player 2";
+    count++;
+  }
 }
+var count = 3;
 
 let testButton = document.querySelector("#test");
 
