b878e4ac578fd12e24060d48eb3fe109335dcc51
Andreas Geckler <ageckler@web.de>
2022-11-20 11:13:38 +0100

    add buttons html

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function choose() {
+function choose1() {
   if (count % 2 == 0) {
     leftTop.style.color = "red";
     leftTop.innerHTML = "Player 1";
@@ -9,8 +9,24 @@ function choose() {
     count++;
   }
 }
-var count = 3;
+function choose2() {
+  if (count % 2 == 0) {
+    centerTop.style.color = "red";
+    centerTop.innerHTML = "Player 1";
+    count++;
+  } else {
+    centerTop.style.color = "blue";
+    centerTop.innerHTML = "Player 2";
+    count++;
+  }
+}
+
+var count = 0;
 
 let testButton1 = document.querySelector("#b1");
 
-testButton1.addEventListener("click", choose);
+testButton1.addEventListener("click", choose1);
+
+let testButton2 = document.querySelector("#b2");
+
+testButton2.addEventListener("click", choose2);
